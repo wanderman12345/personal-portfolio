09c5848366a938ee8a30e38f22dc8baf48d0d519
Mathew Raju <mathew2002raj@gmail.com>
2024-07-08 22:07:39 -0700

    working on sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.scss';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
   return (
    <Routes>
-    <Route path = "/" element = {}
+    <Route path = "/" element = {<Layout />} />
    </Routes>
   );
 }
