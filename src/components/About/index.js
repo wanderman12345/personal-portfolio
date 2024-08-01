d8a141a11df5b058c3d5434ef56ed0c0938d6955
Mathew Raju <mathew2002raj@gmail.com>
2024-08-01 12:50:59 -0700

    Responsive fully

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -73,35 +73,13 @@ const About = () => {
                 </Button>
           </div>
             </div>
-            {/* <div className = 'stage-cube-cont'>
-                <div className  ='cubespinner'>
-                    <div className = 'face1'>
-                        <FontAwesomeIcon icon ={faLinux} color = '#00000' />
-                    </div>
-                    <div className = 'face2'>
-                        <FontAwesomeIcon icon ={faHtml5} color = '#F06529' />
-                    </div>
-                    <div className = 'face3'>
-                        <FontAwesomeIcon icon ={faSass} color = '#CC6699' />
-                    </div>
-                    <div className = 'face4'>
-                        <FontAwesomeIcon icon ={faReact} color = '#5ED4F4' />
-                    </div>
-                    <div className = 'face5'>
-                        <FontAwesomeIcon icon ={faJsSquare} color = '#EFd81D' />
-                    </div>
-                    <div className = 'face6'>
-                        <FontAwesomeIcon icon ={faGitAlt} color = '#EC4D28' />
-                    </div>
-                </div>
-            </div> */}
             <div className='Timeline'>
                     <h2><AnimatedLetters
                         letterClass={letterClass}
                         strArray = {['E', 'd', 'u', 'c', 'a', 't', 'i', 'o', 'n']}
                         idx = {19}
                      /></h2>
-                    <p> My academic journey </p>
+                    <p className = "start"> My academic journey </p>
                     <VerticalTimeline>
                         <VerticalTimelineElement
                             className="vertical-timeline-element--work"
